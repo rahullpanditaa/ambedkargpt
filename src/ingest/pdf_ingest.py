da8523b2d383675459393c07260365c17b0c692b
Rahul Pandita <rpandita1999@outlook.com>
2025-12-11 04:36:45 +0000

    create method to extract/segment paragraphs from raw text

--- src/ingest/pdf_ingest.py
+++ src/ingest/pdf_ingest.py
@@ -10,9 +10,54 @@ RAW_BOOK_TEXT = extract_text(AMBEDKAR_BOOK_PATH)
 class PDFIngestion:
     def __init__(self, pdf=RAW_BOOK_TEXT):
         self.pdf_text = pdf
-        self.pages = []
+
+        # each str is an entire page's text
+        self.pages: list[str] = []
 
     def _split_pdf_into_pages(self):
         # split on form feed characters
         pages = self.pdf_text.split("\x0c")
         self.pages = pages
+
+    # each page of text -> list of paragraphs
+    def _extract_paragraphs(self) -> list[dict]:
+        paragraphs = []
+        for page_number, page_text in enumerate(self.pages, 1):
+
+            # split page into lines
+            lines = page_text.split("\n")
+
+            current_paragraph_lines = []
+            para_idx = 0
+
+            for line in lines:
+                cleaned_line = line.strip()
+
+                if cleaned_line == "":
+                    # if line is blank -> it is a para boundary
+                    if current_paragraph_lines:
+                        merged_text = " ".join(current_paragraph_lines).strip()
+                        paragraphs.append({
+                            "page": page_number,
+                            "para_idx": para_idx,
+                            "text": merged_text
+                        })
+                        para_idx += 1
+                        current_paragraph_lines = []
+
+                    continue
+
+                # non empty line - same para
+                current_paragraph_lines.append(line)
+
+            if current_paragraph_lines:
+                merged_text = " ".join(current_paragraph_lines).strip()
+                paragraphs.append({
+                    "page": page_number,
+                    "para_idx": para_idx,
+                    "text": merged_text
+                })
+        
+        return paragraphs
+
+
