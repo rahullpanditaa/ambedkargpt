d9ad7f3c426e83c2ec8f511eea013fe378a8df3b
Rahul Pandita <rpandita1999@outlook.com>
2025-12-12 06:33:52 +0000

    write method to create chunks; save to json file

--- src/chunking/semantic_chunker.py
+++ src/chunking/semantic_chunker.py
@@ -3,9 +3,14 @@ import numpy as np
 from src.utils.constants import (
     SEGMENTS_DISTANCES_PATH,
     BUFFER_MERGE_RESULTS_PATH,
-    BOOK_SENTENCES_PATH
+    BOOK_SENTENCES_PATH,
+    CHUNKS_OUTPUT_PATH
 )
 THETA = 0.30
+MAX_TOKENS = 1024
+SUBCHUNK_SIZE = 128
+SUBCHUNK_OVERLAP = 32
+
 from transformers import AutoTokenizer
 
 class SemanticChunking:
@@ -49,3 +54,75 @@ class SemanticChunking:
             "text": final_text,
             "num_tokens": num_of_tokens
         }
+    
+    def _split_large_chunk(self, chunk: dict) -> list[dict]:
+
+        if chunk["num_tokens"] <= MAX_TOKENS:
+            return [chunk]
+
+        text = chunk["text"]
+        tokens = self.tokenizer.encode(text)
+
+        subchunks = []
+        start = 0
+
+        while start < len(tokens):
+            end = start + SUBCHUNK_SIZE
+            window_tokens = tokens[start:end]
+            sub_text = self.tokenizer.decode(window_tokens)
+
+            subchunks.append({
+                "sentence_indices": chunk["sentence_indices"],  # optional
+                "text": sub_text,
+                "num_tokens": len(window_tokens)
+            })
+
+            # overlap for continuity
+            start = end - SUBCHUNK_OVERLAP
+
+            if start < 0:
+                break
+
+        return subchunks
+    
+    def create_chunks(self) -> list[dict]:
+        distances = self.segment_distances  
+
+        chunks = []
+        current_chunk_units = [0] 
+
+        for i in range(len(distances)):
+            d = distances[i]
+
+            if d < THETA:
+                current_chunk_units.append(i + 1)
+            else:
+                # semantic break --close chunk, start new one
+                chunks.append(current_chunk_units)
+                current_chunk_units = [i + 1]
+
+        chunks.append(current_chunk_units)
+
+        final_chunks = []
+        chunk_id = 1
+
+        for unit_indices in chunks:
+            chunk_obj = self._reconstruct_chunk_from_sentences(unit_indices)
+
+            # if chunk is large -- split
+            subchunks = self._split_large_chunk(chunk_obj)
+
+            for sc in subchunks:
+                final_chunks.append({
+                    "chunk_id": chunk_id,
+                    "text": sc["text"],
+                    "sentence_indices": sc["sentence_indices"],
+                    "num_tokens": sc["num_tokens"],
+                    "source_units": unit_indices  # helpful for debugging
+                })
+                chunk_id += 1
+                
+        with open(CHUNKS_OUTPUT_PATH, "w") as f:
+            json.dump({"chunks": final_chunks}, f, indent=2)
+
+        return final_chunks
