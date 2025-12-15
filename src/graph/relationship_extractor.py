1d3f06f99b43efa55bdd6442416c78ab7bf7f643
Rahul Pandita <rpandita1999@outlook.com>
2025-12-15 06:09:57 +0000

    create class RelationshipExtractor, method to extract edges from entities

---
+++ src/graph/relationship_extractor.py
@@ -0,0 +1,38 @@
+import json
+from pathlib import Path
+from itertools import combinations
+
+DATA_DIR_PATH = Path(__file__).parent.parent.parent.resolve() / "data"
+PROCESSED_DATA_DIR_PATH = DATA_DIR_PATH / "processed"
+CHUNK_ENTITIES_PATH = PROCESSED_DATA_DIR_PATH / "chunk_entities.json"
+
+
+class RelationshipExtractor:
+    def __init__(self):
+        PROCESSED_DATA_DIR_PATH.mkdir(parents=True, exist_ok=True)
+        with open(CHUNK_ENTITIES_PATH, "r") as f:
+            entitites = json.load(f)["chunk_entities"]
+        
+        self.chunk_entities = list(filter(lambda d: len(d["entities"]) >= 2, entitites))
+        
+    def extract_relations(self):
+        edges = []
+
+        for ch in self.chunk_entities:
+            
+            entitites = ch["entities"]
+
+            en_texts = [e["text_norm"] for e in entitites]
+
+            #           subsequences of length 2 generated from en_texts
+            for a, b in combinations(en_texts, 2):
+                src, trgt = sorted([a, b])
+
+                edges.append({
+                    "source": src,
+                    "target": trgt,
+                    "relation": "co_occurence",
+                    "chunk_id": ch["chunk_id"],
+                    "weight": 1
+                })
+        return edges
